9c67e422b7ccaa7ffb9e2e5304a1dbb2c6a1704a
rob587 <robertocamm02@yahoo.com>
2025-07-21 15:12:15 +0200

    risposta di postman inserita su js

---
+++ js/script.js
@@ -0,0 +1,38 @@
+[
+    {
+        "id": 1,
+        "title": "Skate Park",
+        "date": "01-07-2024",
+        "url": "https://marcolanci.it/boolean/assets/pictures/1.png"
+    },
+    {
+        "id": 2,
+        "title": "Passeggiata",
+        "date": "16-07-2024",
+        "url": "https://marcolanci.it/boolean/assets/pictures/2.png"
+    },
+    {
+        "id": 3,
+        "title": "Alpi",
+        "date": "01-07-2024",
+        "url": "https://marcolanci.it/boolean/assets/pictures/3.png"
+    },
+    {
+        "id": 4,
+        "title": "Sagra",
+        "date": "21-08-2024",
+        "url": "https://marcolanci.it/boolean/assets/pictures/4.png"
+    },
+    {
+        "id": 5,
+        "title": "Watergun",
+        "date": "23-08-2024",
+        "url": "https://marcolanci.it/boolean/assets/pictures/5.png"
+    },
+    {
+        "id": 6,
+        "title": "Riviera",
+        "date": "30-08-2024",
+        "url": "https://marcolanci.it/boolean/assets/pictures/6.png"
+    }
+]
